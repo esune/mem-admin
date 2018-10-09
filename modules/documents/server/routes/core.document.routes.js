adad00efc9dac87068da14dc4e3ec35067d338d8
Emiliano Suñé <emiliano.sune@quartech.com>
2018-10-09 15:28:48 -0700

    Handle temp file removal on failed upload.

--- modules/documents/server/routes/core.document.routes.js
+++ modules/documents/server/routes/core.document.routes.js
@@ -174,6 +174,9 @@ module.exports = function (app) {
               })
             })
             .catch(function(error) {
+              // remove file from temp folder
+              fs.unlink(req.file.path);
+
               // general catch all
               return Promise.reject(error);
             });
@@ -265,6 +268,9 @@ module.exports = function (app) {
               })
             })
             .catch(function(error) {
+              // remove file from temp folder
+              fs.unlink(req.file.path);
+
               // general catch all
               return Promise.reject(error);
             });
